8e3f92d9183487e2eb930b8bc23625d3e95b2cc0
Caio Vinicius <caio.vinnicius2k@gmail.com>
2022-06-12 18:29:08 -0300

    adding responsive style to buttons

--- src/components/WidgetForm/Steps/FeedbackTypeStep.vue
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.vue
@@ -4,13 +4,13 @@
       <span class="text-xl leading-6">Deixe seu feedback</span>
       <CloseButton />
     </header>
-    <div class="flex py-8 gap-2 w-full">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-2 my-4">
       <button
         id="feedback-type-button"
         v-for="(value, key) in feedbackTypes"
         :key="key"
         type="button"
-        class="bg-zinc-800 rounded-lg py-5 w-36 lg:w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition-colors"
+        class="bg-zinc-800 rounded-lg py-5 w-36 lg:w-24 flex-1 flex flex-col items-center mx-auto gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition-colors"
         @click="$emit('onFeedbackTypeChanged', key as FeedbackType)"
       >
         <img :src="value.image.source" :alt="value.image.alt" />
